14f5e0fda807707f9333a62d0f359efd1931ece2
Asif Iqbal <asif.engr13@gmail.com>
2021-02-25 09:50:21 +0100

    recursion

--- Recursion/Excer.js
+++ Recursion/Excer.js
@@ -28,27 +28,33 @@ function findFactorialRecursive(number) {
 
 // Fibonacci
 // Given a number N return the index value of the Fibonacci sequence, where the sequence is:
-
 // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 ...
 // the pattern of the sequence is that each value is the sum of the 2 previous values, that means that for N=5 → 2+3
-
 //For example: fibonacciRecursive(6) should return 8
 
 function fibonacciIterative(n){
-  const fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144];
-  for(let i = 0; i < fibonacci.length; i++){
+  let arr = [0, 1];
 
-    return fibonacci[i];
+  for(let i = 2; i < n +1; i++){
+    arr.push(arr[i-2] + arr[i-1]);
   }
-  }
-  fibonacciIterative(3);
+  return arr[n];
+}
+fibonacciIterative(3);
+//Big O is: O(n)
   
-  function fibonacciRecursive(n) {
-    const fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144];
-    
-    if(n) {
-        return fibonacci[n];
-    }
+
+function fibonacciRecursive(n) {
+  if(n < 2) {
+      return n;
   }
+  return fibonacciRecursive(n-1) + fibonacciRecursive(n-2);
+}
   
-  fibonacciRecursive(3)
+fibonacciRecursive(6)
+//Big O is:if n increases, fn call increases exponentially, O(2 pow N) bad then n*n (two nested fro loop). TimeComplexity. 
+// It can be solved to O(n). - Anything with recursion can be done with iteratively (loop)
+// But recussion can make your code dry, more readable, simpler with extra memory(LargeStack). Not alwasy best approach. 
+// Good for tree datastructure, in doing traversal, BFS, DFS
+// tail call optimization - help to not increase the call stack - in JS(ES6) and LargeStack can be solved during production. 
+// Devide and Conquer using Recursion
