5f259cb9384ded01874738b449cddcf1e266f745
Asif Hoque <asif.engr13@gmail.com>
2021-03-02 22:05:50 +0100

    sort

--- Recursion/Excer.js
+++ Recursion/Excer.js
@@ -1,6 +1,5 @@
 // Write two functions that finds the factorial of any number. 
 //One should use recursive, the other should just use a for loop
-
 function findFactorialRecursive(number) {
     if(number === 2) {
         return 2;
