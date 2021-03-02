bec205ad96493f6481ec02d766df4634195fac50
Asif Hoque <asif.engr13@gmail.com>
2021-03-02 22:04:34 +0100

    sort

--- insertionsort.js
+++ insertionsort.js
@@ -1,6 +1,5 @@
 // when the list is almost/nearly sorted - performs really well for samll datasets
 //comparing each element with the preceding or previous elements except the first one (leaving as it is)
-
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 function insertionSort(array) { 
