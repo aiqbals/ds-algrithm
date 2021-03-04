5293bbffc9ed3c24facb1dd8d42afc4a50407b4e
Asif Hoque <asif.engr13@gmail.com>
2021-03-04 07:20:38 +0100

    stacksqueues

---
+++ StacksQueues/Queue.js
@@ -0,0 +1,2 @@
+// e.g., waiting queue - FIFO
+// Lot of use e.g, online ticket counter, resturant reserv or order food, lift, printer etc. 
---
+++ StacksQueues/Stack.js
@@ -0,0 +1,5 @@
+// Most programming lang are modeled with Sacks architectur in mind - LIFO O(n)
+//e.g., wen fn gets called, usually they follow LIFO O(1)
+// e.g., browser history back and forth, undo-redo O(1)
+// lookup, pop, push, peek O(1)
+
