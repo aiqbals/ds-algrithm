6a6cc411e3c573aad95c03c869dc6fa4e5d86598
Asif Hoque <asif.engr13@gmail.com>
2021-03-04 07:56:31 +0100

    stacksqueues

--- StacksQueues/Queue.js
+++ StacksQueues/Queue.js
@@ -1,2 +1,4 @@
 // e.g., waiting queue - FIFO
 // Lot of use e.g, online ticket counter, resturant reserv or order food, lift, printer etc. 
+// lookpu O(n), enqueue O(1), dequeue O(1), peek O(1)
+// Build with Array or Linkedlist - But Array not a good idea (index shifting)
--- StacksQueues/Stack.js
+++ StacksQueues/Stack.js
@@ -2,4 +2,29 @@
 //e.g., wen fn gets called, usually they follow LIFO O(1)
 // e.g., browser history back and forth, undo-redo O(1)
 // lookup, pop, push, peek O(1)
+// Build with Array(faster access in memory cuz they are next to each oter) / static and dynamic
+// or LinkedList (scattered all over memroy, use extram memory But more dynamic)
 
+class Node {
+    constructor(value){
+      this.value = value;
+      this.next = null;
+    }
+  }
+  
+  class Stack {
+    constructor(){
+      this.top = null;
+      this.bottom = null;
+      this.length = 0;
+    }
+    peek() {
+    }
+    push(value){
+    }
+    pop(){
+    }
+    //isEmpty
+  }
+  
+  const myStack = new Stack();
